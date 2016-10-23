d47383325f82705849174088dfc525fe01e059d6
yucong02 <yucong02@baidu.com>
2016-10-23 21:16:00 +0800

    Update 2016-10-23T13:15:51.030Z

---
+++ mvtmJson_gpull.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ ! -n "$1" ];then
+    echo "no theme chooesed"
+else
+    mv themes/"$1"/theme.config.json themes/"$1".json
+    (cd themes/"$1" && git pull)
+    mv themes/"$1".json themes/"$1"/theme.config.json
+fi
