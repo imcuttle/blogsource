e4e2dc448cbac91ac72b9390699c8b54b0ee6833
yucong02 <yucong02@baidu.com>
2017-10-15 10:33:15 +0800

    2017-10-15 10:33:15

--- themes/refreshing/template/Comps/Header.js
+++ themes/refreshing/template/Comps/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router'
 
-export default ({}) =>
+export default ({publicPath}) =>
     <header>
         <Link to={'/'} className="logo-link">
-            <img src={require('../../assets/logo.png')}/>
+            <img src={`${publicPath}favicon.png`}/>
         </Link>
         <ul className="nav nav-list">
             <li className="nav-list-item"><Link className="nav-list-link" activeClassName={"active"} to={'/posts/1'}>Posts</Link></li>
